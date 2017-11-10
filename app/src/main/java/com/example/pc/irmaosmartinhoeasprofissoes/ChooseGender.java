671cf891b0670eaf2f4db6ade9ff46395f2c12ca
BrunoMPereira <brunop.esac@hotmail.com>
2017-11-10 21:24:07 +0000

    SplashScreen

--- app/src/main/java/com/example/pc/irmaosmartinhoeasprofissoes/ChooseGender.java
+++ app/src/main/java/com/example/pc/irmaosmartinhoeasprofissoes/ChooseGender.java
@@ -1,6 +1,7 @@
 package com.example.pc.irmaosmartinhoeasprofissoes;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -15,6 +16,12 @@ public class ChooseGender extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        try {
+            Thread.sleep(4000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         //Fullscreen
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -24,7 +31,17 @@ public class ChooseGender extends AppCompatActivity {
         int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN;
         decorView.setSystemUiVisibility(uiOptions);
 
-        setContentView(R.layout.activity_chooseGender);
+        setTheme(R.style.AppTheme);
+
+
+        SharedPreferences sharedPref = ChooseGender.this.
+                getSharedPreferences("AppPrefs", Context.MODE_PRIVATE);
+
+        if (sharedPref.contains("gender")) {
+            startActivity(new Intent(getApplicationContext(), MainMenu.class));
+        } else {
+            setContentView(R.layout.activity_choose_gender);
+        }
     }
 
     public void chooseMale()
@@ -32,7 +49,7 @@ public class ChooseGender extends AppCompatActivity {
         SharedPreferences sharedPref = ChooseGender.this.getSharedPreferences("AppPrefs", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
         editor.putInt("gender", 0);
-        editor.commit();
+        editor.apply();
     }
 
     public void chooseFemale()
@@ -40,6 +57,6 @@ public class ChooseGender extends AppCompatActivity {
         SharedPreferences sharedPref = ChooseGender.this.getSharedPreferences("AppPrefs", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPref.edit();
         editor.putInt("gender", 1);
-        editor.commit();
+        editor.apply();
     }
 }
